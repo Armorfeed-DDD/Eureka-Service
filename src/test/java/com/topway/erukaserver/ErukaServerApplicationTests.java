e1f70f8e705d8a6475032109b78fa6a633ce791f
De La Mar <delamarzp@gmail.com>
2023-05-24 21:46:23 -0500

    initial commits

---
+++ src/test/java/com/topway/erukaserver/ErukaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topway.erukaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErukaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
